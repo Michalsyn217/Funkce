08265f5e03387ffde502a4fe3c7ef59a742d9cb6
Michalsyn217 <64131225+Michalsyn217@users.noreply.github.com>
2020-09-25 22:12:47 +0200

    Add files via upload
    Ukol UPG

---
+++ ObsahTrojuhelniku.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+double obsah(double a, double v)
+
+{
+	double S;
+	S = (a * v) / 2;
+	if ( a==0 )
+	{
+		return 0;
+	}
+	else if ( v== 0)
+	{
+		return 0;
+	}
+	else
+	{
+		return S;
+	}
+}
+
+int main()
+{
+	double Strana, vyska, Obsah;
+
+	printf("Napiste delku strany a: ");
+
+	scanf_s("%lf", &Strana);
+
+	printf("Napiste vysku : ");
+
+	scanf_s("%lf", &vyska);
+
+	Obsah = obsah(Strana, vyska);
+
+	printf("Obsah je: %lf", Obsah);
+
+}
